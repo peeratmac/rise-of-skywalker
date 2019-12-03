6a075167df77a9d134b6ed8ab9e6ca030a2417b6
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-03 08:26:11 -0700

    Render LoginForm on main route. Update LoginForm with state.

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      quote: '',
+      rank: 'Youngling'
+    };
+  }
   render() {
     return (
       <div>
@@ -11,15 +19,20 @@ class LoginForm extends Component {
               name='name'
               value={this.state.name}
               placeholder='Enter Your Name, example: John Boyega'
-              onChange={}
             />
             <input
               type='text'
               name='quote'
               value={this.state.quote}
               placeholder='Enter Your Favorite Quote, example: Finn is the best, hello, I am the Force.'
-              onChange={}
             />
+            <select name='rank' value={this.state.rank}>
+              <option value='Youngling'>Youngling</option>
+              <option value='Padawan'>Padawan</option>
+              <option value='Jedi Knight'>Jedi Knight</option>
+              <option value='Jedi Master'>Jedi Master</option>
+            </select>
+            <button type='button'>Submit to Proceed</button>
           </form>
         </div>
       </div>
