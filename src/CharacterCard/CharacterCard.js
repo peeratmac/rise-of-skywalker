d3058ad32fffb5ca88e009dee9c5ce1f24cdad9e
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-04 16:02:35 -0700

    Add the rest of relevant information including episodes each character appears in as a list. Styling needed.

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const CharacterCard = ({ character }) => {
   console.log('Character Card:', character);
+  const episodeApperances = character.films.map((film, i) => (
+    <li key={i}>{film}</li>
+  ));
   return (
     <div>
-      <h1>Name: {character.name}</h1>
+      <h2>Name: {character.name}</h2>
+      <p>Homeworld: {character.homeworld}</p>
+      <p>Population: {character.population}</p>
+      <p>Species: {character.species}</p>
+      <ul>Appear in: {episodeApperances}</ul>
     </div>
   );
 };
