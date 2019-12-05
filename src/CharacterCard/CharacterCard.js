6b5f66b34b94a79b693468020d25be4c69f86917
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-04 19:48:16 -0700

    Add favorite icons to character cards.

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './CharacterCard.css';
+import isFavoriteFalse from '../../src/images/isFavoriteFalse.svg';
+import isFavoriteTrue from '../../src/images/isFavoriteTrue.svg';
 
 const CharacterCard = ({ character }) => {
   console.log('Character Card:', character);
@@ -9,11 +11,16 @@ const CharacterCard = ({ character }) => {
   return (
     <div>
       <div className='card'>
+        <img
+          className={'favorite-image'}
+          src={isFavoriteFalse}
+          alt='favorite icon'
+        />
         <h2>Name: {character.name}</h2>
         <p>Homeworld: {character.homeworld}</p>
         <p>Population: {character.population}</p>
         <p>Species: {character.species}</p>
-        <ul>Appear in: {episodeApperances}</ul>
+        <ul className='episode-list'>Appear in: {episodeApperances}</ul>
         <button className='button' type='button'>
           Favorite
         </button>
