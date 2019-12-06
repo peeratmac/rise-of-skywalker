f8a44c6b2c3a5bf2826604d34e0cf3661be2cd1e
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-05 19:03:42 -0700

    Setup boilerplate for Navigation as a functional Component. This will include user profile data and favorites and other navigation items.

---
+++ src/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.css';
+
+const Navigation = () => {
+  return (
+    <div>
+      <h1>Navigation</h1>
+    </div>
+  );
+};
+
+export default Navigation;
