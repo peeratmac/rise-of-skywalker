961f7e9df10f8b916ac54646d524dac727a50418
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-07 14:10:23 -0700

    Minor loading message text change.

--- src/CharacterContainer/CharacterContainer.js
+++ src/CharacterContainer/CharacterContainer.js
@@ -12,7 +12,8 @@ const CharacterContainer = ({
   if (characters.length === 0) {
     return (
       <React.Fragment>
-        <h1>RISE OF SKYWALKER is working hard for you!</h1>
+        <h2>RISE OF SKYWALKER is working hard for you!</h2>
+        <h2>Enjoy our BB8 acting cute while the page is loading...</h2>
         <img src={loadingImage} alt='Loading Image of BB8' />
       </React.Fragment>
     );
