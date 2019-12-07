2c859f7a58d97583df9accc8b0de2bb2afbe96fc
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-07 13:52:27 -0700

    Add BB8 loading GIF.  Add conditional rendering when characters are loading.

--- src/CharacterContainer/CharacterContainer.js
+++ src/CharacterContainer/CharacterContainer.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import CharacterCard from '../CharacterCard/CharacterCard';
+import loadingImage from '../images/loadingBB8.gif';
 import './CharacterContainer.css';
 
 const CharacterContainer = ({
@@ -8,6 +9,15 @@ const CharacterContainer = ({
   setupFavoriteCharacters,
   favoriteCharactersMap
 }) => {
+  if (characters.length === 0) {
+    return (
+      <React.Fragment>
+        <h1>RISE OF SKYWALKER is working hard for you!</h1>
+        <img src={loadingImage} alt='Loading Image of BB8' />
+      </React.Fragment>
+    );
+  }
+
   const allCharacters = characters.map((character, i) => {
     return (
       <CharacterCard
