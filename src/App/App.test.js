15d0c7fb1366eee5d158a5553211480e083e4fff
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-08 12:39:46 -0700

    Setup the Snapshot UI Testing ready for all components.  Added a Snapshot for App.

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('APP', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('should match the APP Component Snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 });
