b610e2ad985bb2d5fb4c7755c72ff0e17005da26
Naomi Campos <naomicampos@nkcampos.local>
2019-12-09 16:50:14 -0700

    Add test for uploaded character data

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-describe('APP', () => {
+describe.only('APP', () => {
   let wrapper;
 
   beforeEach(() => {
@@ -13,4 +13,22 @@ describe('APP', () => {
     wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  //need to test setupcharacters
+
+  it('should reset the character data state', () => {
+    const startState = {isCharactersDataLoaded: true, selectedCharacters: [{name: 'Leia'}, {name: 'Jabba the Hutt'}] }
+    const endState = {isCharactersDataLoaded: false, selectedCharacters: [] }
+
+    wrapper.setState(startState)
+
+    wrapper.instance().stateUpdateIsCharactersDataLoaded()
+
+    expect(wrapper.state().isCharactersDataLoaded).toEqual(false)
+
+    expect(wrapper.state().selectedCharacters).toEqual([])
+
+  })
+
+
 });
