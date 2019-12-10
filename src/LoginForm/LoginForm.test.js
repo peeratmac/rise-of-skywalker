fa5043d97c70b4954e7834234638d711e3af02bd
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-10 13:57:30 -0700

    Add more tests to login form for handleChange and setupUserProfile to have been called when inputs are not empty.

--- src/LoginForm/LoginForm.test.js
+++ src/LoginForm/LoginForm.test.js
@@ -14,4 +14,56 @@ describe('LOGIN FORM', () => {
   it('should match the LoginForm Component Snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update the appropriate states for user profile when handleChange is called', () => {
+    wrapper = shallow(<LoginForm setupUserProfile={mockSetupUserProfile} />);
+
+    const mockEvent1 = {
+      target: {
+        name: 'name',
+        value: 'Peerat'
+      }
+    };
+
+    const mockEvent2 = {
+      target: {
+        name: 'quote',
+        value:
+          "If there ever comes a day when we can't be together, keep me in your heart. I'll stay there forever."
+      }
+    };
+
+    const mockEvent3 = {
+      target: {
+        name: 'rank',
+        value: 'Jedi Knight'
+      }
+    };
+
+    wrapper.instance().handleChange(mockEvent1);
+    expect(wrapper.state('name')).toEqual('Peerat');
+
+    wrapper.instance().handleChange(mockEvent2);
+    expect(wrapper.state('quote')).toEqual(
+      "If there ever comes a day when we can't be together, keep me in your heart. I'll stay there forever."
+    );
+
+    wrapper.instance().handleChange(mockEvent3);
+    expect(wrapper.state('rank')).toEqual('Jedi Knight');
+  });
+
+  it('should call on setupUserProfile when submit to login to movies page is clicked (handleSubmit)', () => {
+    wrapper = shallow(<LoginForm setupUserProfile={mockSetupUserProfile} />);
+    wrapper.setState({
+      name: 'Peerat',
+      quote: 'Winnie the Pooh!',
+      rank: 'Jedi Knight'
+    });
+    wrapper
+      .find('button')
+      .at(0)
+      .simulate('click');
+
+    expect(mockSetupUserProfile).toHaveBeenCalled();
+  });
 });
