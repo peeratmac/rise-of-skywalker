ffc13d8771c010b048e7581f3550f09a1316a554
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-10 15:24:57 -0700

    Add more state changes and method testing to App Component.

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -58,19 +58,37 @@ describe.only('APP', () => {
     expect(setupCharactersData).toHaveBeenCalled();
   });
 
-  it('should reset the character data state', () => {
+  it('should update state with the call on stateUpdateIsCharactersDataLoaded', () => {
     const startState = {
       isCharactersDataLoaded: true,
       selectedCharacters: [{ name: 'Leia' }, { name: 'Jabba the Hutt' }]
     };
-    const endState = { isCharactersDataLoaded: false, selectedCharacters: [] };
 
     wrapper.setState(startState);
-
     wrapper.instance().stateUpdateIsCharactersDataLoaded();
-
     expect(wrapper.state().isCharactersDataLoaded).toEqual(false);
-
     expect(wrapper.state().selectedCharacters).toEqual([]);
+  });
+
+  it('should be able to updateCrawling text', () => {
+    wrapper = shallow(<App />);
+    wrapper.instance().updateCrawling(7);
+    expect(wrapper.state('crawlingIndex')).toEqual(6);
+  });
+
+  it('should clear favorites upon user signing out', () => {
+    wrapper = shallow(<App />);
+    wrapper.setState({
+      favoriteCharacters: [
+        {
+          name: 'Peerat',
+          species: 'Human',
+          films: ['The Lion King', 'Rise of Skywalker'],
+          homeworld: 'Earth'
+        }
+      ]
+    });
+    wrapper.instance().clearFavoritesUponSignout();
+    expect(wrapper.state('favoriteCharacters')).toEqual([]);
   });
 });
