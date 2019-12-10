a451448efd36f00f7c0d0cf1329a6b3e7dc284de
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-12-09 19:36:57 -0700

    Add more testing to App.test to make sure getAllMoviesData has been called upon mounting.

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { getAllMoviesData, setupCharactersData } from '../apiCalls';
+jest.mock('../apiCalls.js');
 import App from './App';
 
 describe('APP', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<App />);
+    getAllMoviesData.mockImplementation(() => {
+      return Promise.resolve([{}, {}, {}, {}, {}, {}, {}]);
+    });
   });
 
   it('should match the App Component Snapshot', () => {
     wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should getAllMoviesData upon mounting', () => {
+    shallow(<App />);
+    expect(getAllMoviesData).toHaveBeenCalled();
   });
 });
